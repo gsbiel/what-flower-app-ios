4c8aed974c3fdc700050c8b2992f8579fa887456
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-30 20:01:17 -0500

    Implementando o ImagePicker, carregando o modelo do CoreML, executando a logica de predicao e exibindo o resultado para o usuario

--- WhaFlowerIsIt/ViewController.swift
+++ WhaFlowerIsIt/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
+    
+    
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func detectFlower(flowerImage : CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Could not load the model.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (req, error) in
+            guard let results = req.results as? [VNClassificationObservation] else {
+                fatalError("The model failed to process the image.")
+            }
+            
+            if let firstResult = results.first {
+                self.navigationItem.title = "It is a \(firstResult.identifier)"
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print("Error while performing the request.\(error)")
+        }
+    }
+}
 
+extension ViewController : UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let imagePicked = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageView.image = imagePicked
+            
+            guard let ciImage = CIImage(image: imagePicked) else {
+                fatalError("Can not convert image to a CIImage data type.")
+            }
+            
+            detectFlower(flowerImage : ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
 
+extension ViewController: UINavigationControllerDelegate {
+    
 }
 
